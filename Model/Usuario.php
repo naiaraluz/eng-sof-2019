8574eaeb8a69665a58518cd9dbee4141f2761394
unknown <IFSC@CDR-750874.cacador.ifsc.edu.br>
2019-09-18 22:27:50 -0300

    hy

--- Model/Usuario.php
+++ Model/Usuario.php
@@ -9,6 +9,21 @@ class Usuario{
 
     public function logar($login, $senha){
         $conexaoDB = $this->conectarBanco();
+        $sql = $conexaoDB->prepare("select login, nome, logado, celular from usuario
+                                    where
+                                    login = ?
+                                    and
+                                    senha = ?");
+        $sql->bind_param("ss", $login, $senha);
+        $sql->execute();
+
+        $resultado =$sql->get_result();
+        if($resultado->num_rows() ==== "0"){
+          $this->login = null;
+          $this->nome = null;  
+          $this->email = null; 
+          $this->logado = null;
+        }
     }
     private function conectarBanco(){
         $conn = new \mysqli('localhost', 'root', '', 'bd_prospects');
