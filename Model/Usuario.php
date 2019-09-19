fc93c25265a17ab345df530ac99b91f39c2b1684
unknown <IFSC@CDR-750874.cacador.ifsc.edu.br>
2019-09-18 22:27:07 -0300

    hy

--- Model/Usuario.php
+++ Model/Usuario.php
@@ -3,8 +3,16 @@ namespace models;
 
 class Usuario{
     public $login;
-    public $string;
-    public $senha;
+    public $nome;
+    public $logado;
     public $email;
+
+    public function logar($login, $senha){
+        $conexaoDB = $this->conectarBanco();
+    }
+    private function conectarBanco(){
+        $conn = new \mysqli('localhost', 'root', '', 'bd_prospects');
+        return $conn;
+    }
 }
 ?>
