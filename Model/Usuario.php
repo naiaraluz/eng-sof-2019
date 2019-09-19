6ab49976134b6705665c02fbe98bbd61ed0055ec
unknown <IFSC@CDR-750875.cacador.ifsc.edu.br>
2019-09-18 22:33:25 -0300

    incluido a funcao de login na classe usuario

--- Model/Usuario.php
+++ Model/Usuario.php
@@ -1,13 +1,13 @@
 <?php
 namespace models;
 
-class Usuario{
+class Usuario {
     public $login;
     public $nome;
-    public $logado;
     public $email;
+    public $logado;
 
-    public function logar($login, $senha){
+    public function logar($login, $senha) {
         $conexaoDB = $this->conectarBanco();
         $sql = $conexaoDB->prepare("select login, nome, logado, celular from usuario
                                     where
@@ -18,13 +18,24 @@ class Usuario{
         $sql->execute();
 
         $resultado =$sql->get_result();
-        if($resultado->num_rows() ==== "0"){
-          $this->login = null;
-          $this->nome = null;  
-          $this->email = null; 
-          $this->logado = null;
+
+        if($resultado->num_rows() === "0") {
+            $this->login = null;
+            $this->nome = null;  
+            $this->email = null; 
+            $this->logado = false;
+
+        }else {
+            while($linha = $resultado->fetch_assoc()) {
+                $this->login = $linha['login'];
+                $this->nome = $linha['nome'];
+                $this->email = $linha['email'];
+                $this->logado = true;
+            }
         }
+        $sql->close();
     }
+
     private function conectarBanco(){
         $conn = new \mysqli('localhost', 'root', '', 'bd_prospects');
         return $conn;
