b7422ac5ec901c0af55c451f20a6f4a1e6e48f64
unknown <IFSC@CDR-750875.cacador.ifsc.edu.br>
2019-09-20 22:25:01 -0300

    teste 1

--- tests/UsuarioTest.php
+++ tests/UsuarioTest.php
@@ -3,4 +3,19 @@ namespace test;
 
 require_once('..vendor/autoload.php');
 require_once('../Model/Usuario.php');
+
+use models\Usuario;
+use PHPUnit\Framework\TestCase;
+
+class UsuarioTest extends TestCase {
+    /** @test */
+    public function testLogar() {
+        $usuario = new Usuario();
+
+        $this->assertEquals(
+            true,
+            $usuario->logar('paulo', '123')
+        );
+    }
+}
 ?>
